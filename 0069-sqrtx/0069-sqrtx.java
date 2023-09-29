622eb1c50ec361e7a48ac05d5ef544c6043cb7fb
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2023-09-29 23:10:54 +0530

    Time: 1 ms (97.46%), Space: 39.8 MB (32.11%) - LeetHub

---
+++ 0069-sqrtx/0069-sqrtx.java
@@ -0,0 +1,13 @@
+class Solution {
+    public static int mySqrt(int x) {
+        int s;
+        if(x<0)
+            return 0;
+            else
+            {
+                s=(int)Math.sqrt(x);
+            }
+            return s;
+    
+    }
+}
