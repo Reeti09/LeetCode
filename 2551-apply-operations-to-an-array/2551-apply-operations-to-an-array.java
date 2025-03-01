c961bb9ce6230c049805b1cb90436057b9232a5e
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2025-03-01 21:59:31 +0530

    Time: 1 ms (96.37%), Space: 44.4 MB (8.68%) - LeetHub

---
+++ 2551-apply-operations-to-an-array/2551-apply-operations-to-an-array.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int[] applyOperations(int[] nums) {
+        int n = nums.length;
+
+        // Step 1: Apply operations (double value & set next element to zero)
+        for (int i = 0; i < n - 1; i++) {
+            if (nums[i] == nums[i + 1]) {
+                nums[i] *= 2;
+                nums[i + 1] = 0;
+            }
+        }
+
+        // Step 2: Shift all zeros to the end
+        int[] result = new int[n];
+        int index = 0;
+        for (int num : nums) {
+            if (num != 0) {
+                result[index++] = num;
+            }
+        }
+
+        return result;
+    }
+}
