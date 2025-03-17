94497046b953dee6cbdac8baa367f47ef495dcba
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2025-03-17 23:38:26 +0530

    Time: 8 ms (40.3%), Space: 44.6 MB (49.25%) - LeetHub

---
+++ 2308-divide-array-into-equal-pairs/2308-divide-array-into-equal-pairs.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public boolean divideArray(int[] nums) {
+        Map<Integer, Integer> countMap = new HashMap<>();
+        
+        // Count the frequency of each number
+        for (int num : nums) {
+            countMap.put(num, countMap.getOrDefault(num, 0) + 1);
+        }
+
+        // Check if all counts are even
+        for (int count : countMap.values()) {
+            if (count % 2 != 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    
+}
