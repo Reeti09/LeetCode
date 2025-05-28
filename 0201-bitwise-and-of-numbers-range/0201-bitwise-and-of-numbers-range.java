5142d88b6cc45ea2659e56247dd52ac0980a09fd
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2025-05-29 01:08:55 +0530

    Time: 3 ms (100%), Space: 43.7 MB (94.73%) - LeetHub

---
+++ 0201-bitwise-and-of-numbers-range/0201-bitwise-and-of-numbers-range.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int rangeBitwiseAnd(int m, int n) {
+        int shift=0;
+        while(m!=n){
+            m=m>>1;
+            n=n>>1;
+            shift++;
+        }
+        return m<<shift;
+        
+    }
+}
