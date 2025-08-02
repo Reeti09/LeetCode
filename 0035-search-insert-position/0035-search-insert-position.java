e30e2e9b99c5d1dce3bbcd4afbb9539a322c996b
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2025-08-02 18:42:40 +0530

    Time: 0 ms (100%), Space: 42.9 MB (66.14%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,10 +1,17 @@
 class Solution {
     public int searchInsert(int[] nums, int target) {
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] >= target) {
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]==target){
                 return i;
             }
+            else{
+                if(nums[i]>target){
+                    return i;
+                    
+                }
+            }
+            
         }
-        return nums.length; // insert at end if target is greater than all elements
+        return nums.length;
     }
 }
