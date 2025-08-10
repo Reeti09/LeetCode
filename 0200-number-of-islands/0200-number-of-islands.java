fb78ba78e5338d6b3c77296369b88230e6e58c55
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2025-08-10 11:09:33 +0530

    Time: 2 ms (99.8%), Space: 49.8 MB (40.3%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.java
+++ 0200-number-of-islands/0200-number-of-islands.java
@@ -1,52 +1,29 @@
 class Solution {
     public int numIslands(char[][] grid) {
-        int n=grid.length;
-        int m=grid[0].length;
-        boolean[][] vis=new boolean[n][m];
-        int c=0;
-        for(int i=0;i<n;i++){
-            for(int j=0;j<m;j++){
-                if(!vis[i][j] && grid[i][j]=='1'){
-                    c++;
-                    bfs(i,j,vis,grid);
+        if(grid==null || grid.length==0) return 0;
+        int numIslands=0;
+        int rows=grid.length;
+        int cols=grid[0].length;
+        for(int r=0;r<rows;r++){
+            for(int c=0;c<cols;c++){
+                if(grid[r][c]=='1'){
+                    numIslands++;
+                    dfs(grid, r, c);
                 }
             }
         }
-        return c;
+        return numIslands;
         
     }
-    private void bfs(int i, int j, boolean[][] vis, char[][] grid) {
-    vis[i][j] = true;
-    Queue<int[]> q = new LinkedList<>();
-    q.add(new int[]{i, j});
-    int n = grid.length;
-    int m = grid[0].length;
-
-    // Only 4 directions: up, down, left, right
-    int[] delRow = {-1, 0, 1, 0};
-    int[] delCol = {0, -1, 0, 1};
-
-    while (!q.isEmpty()) {
-        int[] cell = q.poll();
-        int row = cell[0];
-        int col = cell[1];
-
-        for (int k = 0; k < 4; k++) {
-            int newRow = row + delRow[k];
-            int newCol = col + delCol[k];
-
-            if (isValid(newRow, newCol, n, m) && grid[newRow][newCol] == '1' && !vis[newRow][newCol]) {
-                vis[newRow][newCol] = true;
-                q.add(new int[]{newRow, newCol});
-            }
+    private void dfs(char[][] grid, int r, int c){
+        if(r<0|| r>=grid.length|| c<0 ||c>=grid[0].length){
+            return ;
         }
-    }
-}
-
-    
-    private boolean isValid(int i, int j, int n, int m){
-        if(i<0 || i>=n) return false;
-        if(j<0 || j>=m) return false;
-        return true;
+        if(grid[r][c]=='0') return;
+        grid[r][c]='0';
+        dfs(grid, r+1, c);
+        dfs(grid, r-1, c);
+        dfs(grid, r, c+1);
+        dfs(grid, r, c-1);
     }
 }
