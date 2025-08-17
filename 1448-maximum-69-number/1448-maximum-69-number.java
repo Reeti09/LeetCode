09c043ec3cb2ab4bc91077f0f36e35f19fab88ac
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2025-08-17 07:35:18 +0530

    Time: 2 ms (17.72%), Space: 41.1 MB (5.76%) - LeetHub

--- 1448-maximum-69-number/1448-maximum-69-number.java
+++ 1448-maximum-69-number/1448-maximum-69-number.java
@@ -1,8 +1,12 @@
 class Solution {
-    public int maximum69Number (int num) {
-        String a=String.valueOf(num);
-        a=a.replaceFirst("6","9");
-        return Integer.parseInt(a);
+    public int maximum69Number(int num) {
+        // Convert the integer num to a String
+        String numberAsString = String.valueOf(num);
 
+        // Replace the first occurrence of the character '6' with '9' in the string
+        String modifiedNumber = numberAsString.replaceFirst("6", "9");
+
+        // Convert the modified string back to an integer and return it
+        return Integer.valueOf(modifiedNumber);
     }
 }
