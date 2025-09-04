32129cc26a28a82c2231667b66939a27902c5c77
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2025-09-05 00:10:52 +0530

    Time: 1 ms (100%), Space: 42.2 MB (94.13%) - LeetHub

--- 1036-rotting-oranges/1036-rotting-oranges.java
+++ 1036-rotting-oranges/1036-rotting-oranges.java
@@ -1,43 +1,38 @@
-class Solution {
-    public int orangesRotting(int[][] grid) {
-        if(grid==null || grid.length==0) return 0;
-        int rows=grid.length;
-        int cols=grid[0].length;
-        Queue<int[]> q=new LinkedList<>();
-        int fresh=0;
-        for(int r=0;r<rows;r++){
-            for(int c=0;c<cols;c++){
-                if(grid[r][c]==2){
-                    q.add(new int[]{r,c});
-                }
-                else if(grid[r][c]==1){
-                    fresh++;
-                }
-            }
-        }
-        if(fresh==0) return 0;
-        int minutes=0;
-        int[][] directions={{0,1},{0,-1},{1,0},{-1,0}};
-        while(!q.isEmpty() && fresh>0){
-            minutes++;
-            int size=q.size();
-            for(int i=0;i<size;i++){
-                int[] rotten=q.poll();
-                int r=rotten[0];
-                int c=rotten[1];
-                for(int[] dir: directions){
-                    int newR=r+dir[0];
-                    int newC=c+dir[1];
-                    if(newR>=0 && newR<rows && newC>=0 && newC<cols && grid[newR][newC]==1){
-                        grid[newR][newC]=2;
-                        fresh--;
-                        q.add(new int[]{newR, newC});
+import java.util.Arrays;
 
-                    }
-                }
-            }
+class Solution {
+  public int orangesRotting(int[][] grid) {
+    if (grid == null || grid.length == 0) return -1;
+
+    int rows = grid.length, cols = grid[0].length;
+    int[][] time = new int[rows][cols];
+    for (int i = 0; i < rows; i++)
+      Arrays.fill(time[i], Integer.MAX_VALUE);
+
+    for (int i = 0; i < rows; i++)
+      for (int j = 0; j < cols; j++)
+        if (grid[i][j] == 2)
+          dfs(grid, time, i, j, 0);
+
+    int timeRequired = 0;
+    for (int i = 0; i < rows; i++)
+      for (int j = 0; j < cols; j++)
+        if (grid[i][j] == 1) {
+          if (time[i][j] == Integer.MAX_VALUE) return -1;
+          timeRequired = Math.max(timeRequired, time[i][j]);
         }
-        return fresh==0?minutes: -1;
-        
-    }
+
+    return timeRequired;
+  }
+
+  private void dfs(int[][] grid, int[][] time, int i, int j, int currentTime) {
+    if (i < 0 || i >= grid.length || j < 0 || j >= grid[0].length
+        || grid[i][j] == 0 || currentTime >= time[i][j]) return;
+
+    time[i][j] = currentTime;
+    dfs(grid, time, i - 1, j, currentTime + 1);
+    dfs(grid, time, i + 1, j, currentTime + 1);
+    dfs(grid, time, i, j - 1, currentTime + 1);
+    dfs(grid, time, i, j + 1, currentTime + 1);
+  }
 }
