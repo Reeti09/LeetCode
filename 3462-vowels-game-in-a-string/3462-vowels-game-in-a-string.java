c947a33db06e6902ad405abc4f246b08ad384d53
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2025-09-12 11:15:20 -0700

    Time: 12 ms (28.87%), Space: 45.9 MB (16.8%) - LeetHub

---
+++ 3462-vowels-game-in-a-string/3462-vowels-game-in-a-string.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public boolean doesAliceWin(String s) {
+        for (char c : s.toCharArray()) {
+            if (isVowel(c)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean isVowel(char c) {
+        return "aeiou".indexOf(c) != -1;
+    }
+}
