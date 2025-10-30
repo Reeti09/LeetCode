3c6db6a34c35bd619afa77e820a04a331bf1c104
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2025-10-30 18:18:22 +0530

    Time: 0 ms (100%), Space: 40.6 MB (29.89%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,20 +1,12 @@
 class Solution {
     public int climbStairs(int n) {
-        if(n==1) return 1;
-        if(n==2) return 2;
-        int oneStep=1;
-        int twoStep=2;
-        int total=0;
-        for(int i=3;i<=n;i++)
-        {
-            total=oneStep+ twoStep;
-            oneStep=twoStep;
-            twoStep=total;
+        int[] dp=new int[n+1];
+        dp[0]=1;
+        dp[1]=1;
+        for(int i=2;i<=n;i++){
+            dp[i]=dp[i-1]+dp[i-2];
+
         }
-        return total;
-
-
-
+        return dp[n];
     }
-    
 }
