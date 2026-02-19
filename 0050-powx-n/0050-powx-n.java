3f90e6ae2728849ba9329f37ac7c151d66710016
Reeti Chopra <reetichopra2022@vitbhopal.ac.in>
2026-02-19 19:44:39 +0530

    Time: 0 ms (100%), Space: 48.2 MB (33.96%) - LeetHub

--- 0050-powx-n/0050-powx-n.java
+++ 0050-powx-n/0050-powx-n.java
@@ -1,8 +1,7 @@
 class Solution {
     public double myPow(double x, int n) {
-        double n1=n;
-        double sum=Math.pow(x,n1);
-        return sum;
+        double result=Math.pow(x,n);
+        return result;
         
     }
 }
